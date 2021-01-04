f96ef5581de3b5589f19a44bd6140e33a87c057c
alessandro_montaldo <ale.montaldo@gmail.com>
2021-01-04 20:17:55 +0100

    day 11 solution improved

--- 11.py
+++ 11.py
@@ -1,12 +1,5 @@
 # day 11 (~120 min)
 
-# Represents the following neighborhood system: (where O are neighbors of c)
-#       O O O
-#       O c O
-#       O O O
-neighborhood_relative_index = [(y, x) for y in range(-1, 2) for x in range(-1, 2)]
-neighborhood_relative_index.remove((0, 0))
-
 
 def main():
     data = get_data()
@@ -28,21 +21,10 @@ def main():
     print(count_occupied(data))
 
 
-def fill_margin(data):
-    """Adds floor in the four directions"""
-    data = [['.'] + lst + ['.'] for lst in data]
-    floor_line = []
-    for i in range(len(data[0])):
-        floor_line.append('.')
-    data.insert(0, floor_line)
-    data.append(floor_line)
-    return data
-
-
 def apply_rule(data, predicate, mark_char):
     seats = set()  # set of (y, x) positions of seats to be occupied
-    for y in range(1, len(data) - 1):
-        for x in range(1, len(data[0]) - 1):
+    for y in range(len(data)):
+        for x in range(len(data[0])):
             if predicate(data, y, x) and data[y][x] != '.':
                 seats.add((y, x))
     return mark_seats(data, seats, mark_char)
@@ -65,75 +47,26 @@ def adjacent_occupied_part_2(data, yc, xc, num=5):
 
 
 def get_close_neighbors(data, yc, xc):
-    global neighborhood_relative_index
     neighbors = []
-    for y, x in neighborhood_relative_index:
-        neighbors.append(data[yc + y][xc + x])
+    num_rows, num_cols = len(data), len(data[0])
+    for dy, dx in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+        y, x = yc + dy, xc + dx
+        if 0 <= y < num_rows and 0 <= x < num_cols:
+            neighbors.append(data[y][x])
     return neighbors
 
 
 def get_far_neighbors(data, yc, xc):
     neighbors = []
-    # up
-    y, x = yc - 1, xc
-    while data[y][x] == '.':
-        if y == 0:
-            break
-        y = y - 1
-    neighbors.append(data[y][x])
-    # up-right
-    y, x = yc - 1, xc + 1
-    while data[y][x] == '.':
-        if y == 0 or x == len(data[0]) - 1:
-            break
-        y = y - 1
-        x = x + 1
-    neighbors.append(data[y][x])
-    # right
-    y, x = yc, xc + 1
-    while data[y][x] == '.':
-        if x == len(data[0]) - 1:
-            break
-        x = x + 1
-    neighbors.append(data[y][x])
-    # down-right
-    y, x = yc + 1, xc + 1
-    while data[y][x] == '.':
-        if y == len(data) - 1 or x == len(data[0]) - 1:
-            break
-        y = y + 1
-        x = x + 1
-    neighbors.append(data[y][x])
-    # down
-    y, x = yc + 1, xc
-    while data[y][x] == '.':
-        if y == len(data) - 1:
-            break
-        y = y + 1
-    neighbors.append(data[y][x])
-    # down-left
-    y, x = yc + 1, xc - 1
-    while data[y][x] == '.':
-        if y == len(data) - 1 or x == 0:
-            break
-        y = y + 1
-        x = x - 1
-    neighbors.append(data[y][x])
-    # left
-    y, x = yc, xc - 1
-    while data[y][x] == '.':
-        if x == 0:
-            break
-        x = x - 1
-    neighbors.append(data[y][x])
-    # up-left
-    y, x = yc - 1, xc - 1
-    while data[y][x] == '.':
-        if y == 0 or x == 0:
-            break
-        y = y - 1
-        x = x - 1
-    neighbors.append(data[y][x])
+    num_rows, num_cols = len(data), len(data[0])
+    for dy, dx in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+        y, x = yc + dy, xc + dx
+        while 0 <= y < num_rows and 0 <= x < num_cols:
+            if data[y][x] != '.':
+                neighbors.append(data[y][x])
+                break
+            y += dy
+            x += dx
     return neighbors
 
 
@@ -152,8 +85,7 @@ def count_occupied(data):
 
 def get_data():
     with open('input11.txt', 'r') as f:
-        data = [list(s) for s in f.read().split('\n')]
-        return fill_margin(data)
+        return [list(s) for s in f.read().splitlines()]
 
 
 if __name__ == "__main__":
